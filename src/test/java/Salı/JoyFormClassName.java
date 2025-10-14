a520d60d6da8675e486e4967c4212d139dc255f9
Tolga Aktaş <akts.tlga@gmail.com>
2025-10-14 23:33:05 +0300

    Hem Classname kullanıp hem de FindElements kullanarak birden fazla elemanın getText'ini alıp yazdırma işlemini List aracılığıyla yaptık.

--- src/test/java/Salı/JoyFormClassName.java
+++ src/test/java/Salı/JoyFormClassName.java
@@ -1,9 +1,12 @@
 package Salı;
 
+import Utility.Function;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
 
 public class JoyFormClassName {
     public static void main(String[] args) {
@@ -12,5 +15,16 @@ public class JoyFormClassName {
 
         WebElement element = driver.findElement(By.className("form-sub-label"));
         System.out.println("Element.getText() = " + element.getText());
+        // findElement: locator birden fazla elemanı işaret ederse, sadece ilkini alır.
+
+        List<WebElement> labels = driver.findElements(By.className("form-sub-label"));
+        System.out.println(labels.size());
+
+        for(WebElement e: labels){
+            System.out.println(e.getText());
+        }
+
+        Function.Wait(2);
+        driver.quit();
     }
 }
