8aede42b32060ecd193247a883005f4cc5401d2a
Tolga Aktaş <akts.tlga@gmail.com>
2025-10-14 22:50:16 +0300

    ID kullanarak Search kutusunu bulduk. İçeriğine de Portakal yazdık.

---
+++ src/test/java/Salı/IdIleBulma.java
@@ -0,0 +1,17 @@
+package Salı;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class IdIleBulma {
+    public static void main(String[] args) {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.akakce.com/");
+
+        WebElement aramaKutusu = driver.findElement(By.id("q")); //Q id ile yazılmış olan elemanı bul.
+        aramaKutusu.sendKeys("Portakal");
+
+    }
+}
